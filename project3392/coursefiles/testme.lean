067e23ad225a9ac86e06cf073cd35253c5db30dd
straubin <59456673+straubin@users.noreply.github.com>
2024-08-26 13:41:58 -0400

    Add files via upload

--- project3392/coursefiles/testme.lean
+++ project3392/coursefiles/testme.lean
@@ -1,1 +1,18 @@
 import Mathlib.Tactic
+
+/- a formalized natural deduction proof from the start of the
+class
+-/
+
+variable (car bus rain umbrella : Prop )
+
+example (h₁: car ∨ bus )(h₂: bus ∧ rain → umbrella)(h₃: rain ∧ ¬umbrella):car :=
+by
+cases' h₁ with hc hb  /- we'll use or elimination to produce the conclusion
+                      in for both modes of transport, This produces two
+                      goals. -/
+exact hc /-it's trivial if we assume he took the car!-/
+
+/-if he took the bus, we need a contradiction-/
+have hr: rain := And.left h₃
+have hbr: bus ∧ rain
